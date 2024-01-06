542d70e2e367e5e7aa659d4f6f6e8de87fadc5c7
Janbolot7 <jntrv7.7@gmail.com>
2024-01-06 22:14:06 +0600

    findAll 6

--- src/main/java/kg/neo/mobimarket_2/repository/ProductRepository.java
+++ src/main/java/kg/neo/mobimarket_2/repository/ProductRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findAllByUser_userId(Integer userId);
+    List<Product> findAllByUser_userId(Integer user_id);
 
 }
