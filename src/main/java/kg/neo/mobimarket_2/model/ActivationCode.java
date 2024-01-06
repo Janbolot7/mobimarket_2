d5b5ad61addcb64be11cc400da0ee192eb16c90f
Janbolot7 <jntrv7.7@gmail.com>
2024-01-06 23:36:46 +0600

    add component

--- src/main/java/kg/neo/mobimarket_2/model/ActivationCode.java
+++ src/main/java/kg/neo/mobimarket_2/model/ActivationCode.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import java.util.UUID;
 
+@Component
 @Getter
 @Setter
 @NoArgsConstructor
