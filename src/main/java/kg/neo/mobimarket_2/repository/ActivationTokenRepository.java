5868e73182e65050641df546dc725443b074f74b
Janbolot7 <jntrv7.7@gmail.com>
2024-01-07 12:00:18 +0600

    delete ActivationTokenRepository.java

--- src/main/java/kg/neo/mobimarket_2/repository/ActivationTokenRepository.java
+++ src/main/java/kg/neo/mobimarket_2/repository/ActivationTokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface ActivationTokenRepository extends JpaRepository<ActivationCode, Long> {
-    Optional<ActivationCode> findByToken(String Token);
-}
+//@Repository
+//public interface ActivationTokenRepository extends JpaRepository<ActivationCode, Long> {
+//    Optional<ActivationCode> findByToken(String Token);
+//}
