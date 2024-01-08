2e498809a603ec1a8f0b6ef60ef47ac3d5041603
Janbolot7 <jntrv7.7@gmail.com>
2024-01-08 20:01:14 +0600

    delete email config

--- src/main/java/kg/neo/mobimarket_2/configuration/EmailConfig.java
+++ src/main/java/kg/neo/mobimarket_2/configuration/EmailConfig.java
@@ -8,25 +8,25 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-@Configuration
-public class EmailConfig {
-    @Value("${gmail}")
-    private String mail;
-    @Value("${spring.mail.password}")
-    private String code;
-
-    @Bean
-    public JavaMailSender getJavaMailSender(){
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername(mail);
-        mailSender.setPassword(code);
-
-        Properties properties = mailSender.getJavaMailProperties();
-        properties.put("mail.transport.protocol","smtp");
-        properties.put("mail.smtp.starttls.enable","true");
-        properties.put("mail.debug","true");
-        return mailSender;
-    }
-}
+//@Configuration
+//public class EmailConfig {
+//    @Value("${gmail}")
+//    private String mail;
+//    @Value("${spring.mail.password}")
+//    private String code;
+//
+//    @Bean
+//    public JavaMailSender getJavaMailSender(){
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//        mailSender.setUsername(mail);
+//        mailSender.setPassword(code);
+//
+//        Properties properties = mailSender.getJavaMailProperties();
+//        properties.put("mail.transport.protocol","smtp");
+//        properties.put("mail.smtp.starttls.enable","true");
+//        properties.put("mail.debug","true");
+//        return mailSender;
+//    }
+//}
